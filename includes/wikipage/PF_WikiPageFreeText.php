cb6cf8de0f2ea920202b16b2e1dee04264c2ccc1
Julien <julien@dokit.io>
2019-05-10 15:07:18 +0200

    1016 - [Tag Translate] - Bug quand j'ajoute du contenu juste après un tag Translate

--- includes/wikipage/PF_WikiPageFreeText.php
+++ includes/wikipage/PF_WikiPageFreeText.php
@@ -27,7 +27,15 @@ class PFWikiPageFreeText {
 					$text = $matches[1];
 				}
 			} else {
-				// put them back
+				// fix : rearrange <!--T:<number>--> tags to avoid errors
+				// 4 cases : 
+				//		1 : text<tag> => text\n\n<tag>
+				//		2 : <tag>text => <tag>\ntext
+				//		3 : <tag>\n* => <tag>\n\n
+				//		4 : \n*<tag> => \n\n<tag>
+				$text = preg_replace(['#([^\s])(<!--T:\d+-->)#m', '#(<!--T:\d+-->)([^\s])#m', '#(<!--T:\d+-->)([\s]+)#m', '#([\s]+)(<!--T:\d+-->)#m'], ["$1\n\n$2", "$1\n$2", "$1\n", "\n\n$2"], $text);
+
+				// put translate tags back
 				$text = '<translate>' . $text .'</translate>';
 			}
 		}
